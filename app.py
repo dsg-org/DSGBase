e661f92e294705df8ba5d789840115a41b30fd04
Gurjaka <esartia.gurika@gmail.com>
2025-06-11 19:34:08 +0400

    update: fix main placement

--- app.py
+++ app.py
@@ -11,18 +11,21 @@ process_lock = threading.Lock()
 @app.route("/")
 def base():
     return render_template("base.html")
+
+
 @app.route("/about")
 def about():
     return render_template("/About/about.html")
 
+
 @app.route("/description")
 def description():
     return render_template("/Description/description.html")
+
+
 @app.route("/settings")
 def settings():
     return render_template("/Settings/settings.html")
-
-
 
 
 @app.route("/api/search", methods=["POST"])
@@ -37,7 +40,7 @@ def api_search():
     data = request.json or {}
 
     base_path = os.path.dirname(os.path.abspath(__file__))
-    binary_path = os.path.join(base_path, "src","main")  #main scr ში არის და მაგიტო  
+    binary_path = os.path.join(base_path, "main")
     data_path = os.path.join(base_path, "src", "output.bin")
 
     args = [binary_path, "-f", data_path]
@@ -54,13 +57,13 @@ def api_search():
         result = subprocess.run(args, capture_output=True, text=True, timeout=300)
         raw_output = result.stdout.strip()
 
-        print("Raw output from binary:", raw_output)  
+        print("Raw output from binary:", raw_output)
 
         json_start = raw_output.find("[")
         json_end = raw_output.rfind("]") + 1
         json_part = raw_output[json_start:json_end].strip()
 
-        print("Extracted JSON:", json_part)  
+        print("Extracted JSON:", json_part)
 
         if not json_part.startswith("[") or not json_part.endswith("]"):
             return {"error": "Invalid response format from the search process."}, 500
@@ -68,14 +71,12 @@ def api_search():
         parsed = json.loads(json_part)
         return jsonify(parsed)
 
-
     except subprocess.TimeoutExpired:
         return {"error": "Search timed out after 5 minutes."}, 504
 
     except Exception as e:
-        print("Subprocess failed:", e)  
+        print("Subprocess failed:", e)
         return {"error": f"Internal server error: {str(e)}"}, 500
-
 
     finally:
         process_lock.release()
